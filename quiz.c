4ca19847a3b91ed9323ba368e37c604d8dae4be9
Brian Espina <espinabrian@gmail.com>
2025-11-14 21:51:18 +0800

    refactored to dynamic question list array

--- quiz.c
+++ quiz.c
@@ -12,6 +12,7 @@ typedef struct Game {
   size_t score;
   size_t dificulty;
   question_t *question_list;
+  int question_count;
 } game_t;
 
 game_t *create_game() {
@@ -20,20 +21,26 @@ game_t *create_game() {
     perror("malloc");
     exit(EXIT_FAILURE);
   }
-  question_t *question_list = malloc(sizeof *question_list);
-  if (question_list) {
-    perror("malloc");
-    exit(EXIT_FAILURE);
-  }
 
   new->score = 0;
   new->dificulty = 1;
-  new->question_list = question_list;
-
+  new->question_list = NULL;
+  new->question_count = 0;
   return new;
 }
 
-void append_question(game_t *game, question_t question) {}
+void append_question(game_t *game, question_t q) {
+  question_t *p = realloc(game->question_list,
+                          sizeof(question_t) * (game->question_count + 1));
+  if (!p) {
+    perror("realloc");
+    exit(EXIT_FAILURE);
+  }
+  game->question_list = p;
+  *(game->question_list + game->question_count) = q;
+
+  game->question_count++;
+}
 
 void add_question(game_t *game, char *label, char *answer, size_t dificulty) {
   question_t q;
@@ -46,6 +53,11 @@ int main(void) {
   game_t *g = create_game();
   add_question(g, "test", "test", 1);
   add_question(g, "tiearnstina", "risntaioerste", 1);
+
+  printf("%d\n", g->question_count);
+
+  printf("%s\n", g->question_list[0].label);
+  printf("%s\n", g->question_list[1].label);
 
   free(g);
   return EXIT_SUCCESS;
